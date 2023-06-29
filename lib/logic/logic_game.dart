d85fb3c8236843d3081d84538f0e8b3a157b8acc
Kirill <motovilink@yandex.ru>
2023-06-29 18:15:34 +0300

    Переписал функцию проверки победы в игре

--- lib/logic/logic_game.dart
+++ lib/logic/logic_game.dart
@@ -34,78 +34,30 @@ class LogicGame extends ChangeNotifier {
   }
 
   void _checkWinner() {
-    // первая строка
-    if (displayXO[0] == displayXO[1] &&
-        displayXO[0] == displayXO[2] &&
-        displayXO[0] != '') {
-      resultDeclaration = 'Игрок ${displayXO[0]} выиграл';
-      matchedIndexes.addAll([0, 1, 2]);
-      _stopTimer();
-      _updateScore(displayXO[0]);
+    final List<List<int>> winCondition = [
+      [0, 1, 2], // первая строка
+      [3, 4, 5], // вторая строка
+      [6, 7, 8], // третья строка
+      [0, 3, 6], // первая колонка
+      [1, 4, 7], // вторая колонка
+      [2, 5, 8], // третья колонка
+      [0, 4, 8], // диагональ сверху вниз
+      [6, 4, 2], // диагональ снизу вверх
+    ];
+
+    for (var condition in winCondition) {
+      var elementCondition = condition[0];
+      if (displayXO[condition[0]] != '' &&
+          displayXO[condition[0]] == displayXO[condition[1]] &&
+          displayXO[condition[0]] == displayXO[condition[2]]) {
+        resultDeclaration = 'Игрок ${displayXO[elementCondition]} выиграл';
+        matchedIndexes.addAll(condition);
+        _stopTimer();
+        _updateScore(displayXO[elementCondition]);
+        return;
+      }
     }
-    // вторая строка
-    if (displayXO[3] == displayXO[4] &&
-        displayXO[3] == displayXO[5] &&
-        displayXO[3] != '') {
-      resultDeclaration = 'Игрок ${displayXO[3]} выиграл';
-      matchedIndexes.addAll([3, 4, 5]);
-      _stopTimer();
-      _updateScore(displayXO[3]);
-    }
-    // третья строка
-    if (displayXO[6] == displayXO[7] &&
-        displayXO[6] == displayXO[8] &&
-        displayXO[6] != '') {
-      resultDeclaration = 'Игрок ${displayXO[6]} выиграл';
-      matchedIndexes.addAll([6, 7, 8]);
-      _stopTimer();
-      _updateScore(displayXO[6]);
-    }
-    // первая колонка
-    if (displayXO[0] == displayXO[3] &&
-        displayXO[0] == displayXO[6] &&
-        displayXO[0] != '') {
-      resultDeclaration = 'Игрок ${displayXO[0]} выиграл';
-      matchedIndexes.addAll([0, 3, 6]);
-      _stopTimer();
-      _updateScore(displayXO[0]);
-    }
-    // вторая колонка
-    if (displayXO[1] == displayXO[4] &&
-        displayXO[1] == displayXO[7] &&
-        displayXO[1] != '') {
-      resultDeclaration = 'Игрок ${displayXO[1]} выиграл';
-      matchedIndexes.addAll([1, 4, 7]);
-      _stopTimer();
-      _updateScore(displayXO[1]);
-    }
-    // третья колонка
-    if (displayXO[2] == displayXO[5] &&
-        displayXO[2] == displayXO[8] &&
-        displayXO[2] != '') {
-      resultDeclaration = 'Игрок ${displayXO[2]} выиграл';
-      matchedIndexes.addAll([2, 5, 8]);
-      _stopTimer();
-      _updateScore(displayXO[2]);
-    }
-    // диагональ сверху вниз
-    if (displayXO[0] == displayXO[4] &&
-        displayXO[0] == displayXO[8] &&
-        displayXO[0] != '') {
-      resultDeclaration = 'Игрок ${displayXO[0]} выиграл';
-      matchedIndexes.addAll([0, 4, 8]);
-      _stopTimer();
-      _updateScore(displayXO[0]);
-    }
-    // диагональ снизу вверх
-    if (displayXO[6] == displayXO[4] &&
-        displayXO[6] == displayXO[2] &&
-        displayXO[6] != '') {
-      resultDeclaration = 'Игрок ${displayXO[6]} выиграл';
-      matchedIndexes.addAll([6, 4, 2]);
-      _stopTimer();
-      _updateScore(displayXO[6]);
-    }
+
     if (!winnerFound && filledBoxes == 9) {
       resultDeclaration = 'Ничья';
       _stopTimer();
